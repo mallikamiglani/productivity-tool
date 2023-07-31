ae5671de821ca433c613ce1a75356fd4e0cdc83b
mallikam <mallikam@umich.edu>
2023-07-31 17:23:48 +0530

    displayed cards, handled on drag end

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,16 +7,53 @@ import Column from './Column';
 
 function Board() {
 
-    const [board, getBoard] = useBoardStore((state) => [state.board, state.getBoard]);
+    const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
+        state.board, 
+        state.getBoard, 
+        state.setBoardState, 
+        state.updateTodoInDB]);
     useEffect(() => {
         getBoard()
     }, [getBoard]);
 
+    // droppable id = index of draggable area, index = index within droppable id
     const handleOnDragEnd = (result: DropResult) => {
+        const {destination,source, type} = result;
 
+        // if dragged outside board
+        if (!destination) return;
+
+        // column dragged
+        if(type == 'Column'){
+            const originalColumns = Array.from(board.columns.entries());
+            const [moved] = originalColumns.splice(source.index, 1);
+            originalColumns.splice(destination.index, 0, moved);
+            setBoardState({ ...board, columns: new Map(originalColumns)});
+        }
+        // card dragged
+        else{
+            const sourceCol = Number(source.droppableId);
+            const destCol = Number(destination.droppableId);
+
+            const indexInSourceCol = source.index;
+            const indexInDestCol = destination.index;
+
+            // moved to invalid place / not moved at all
+            if (source.droppableId == null || destination.droppableId == null 
+                || (sourceCol == destCol && indexInSourceCol == indexInDestCol)) 
+                return;
+
+            // changing card order
+            const cardOrder = Array.from(board.columns.entries());
+            const [moved] = cardOrder[sourceCol][1].todos.splice(indexInSourceCol, 1);
+            cardOrder[destCol][1].todos.splice(indexInDestCol, 0, moved);
+            setBoardState({...board, columns: new Map(cardOrder)});
+
+            // update status in database
+            updateTodoInDB(moved, cardOrder[destCol][0]);
+        }
     }
 
-    console.log(board);
     return (
         <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId='board' direction='horizontal' type='Column'>
